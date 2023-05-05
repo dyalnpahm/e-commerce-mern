80e1baba9846c39430cd0c36ebecb1c1cfab4f4e
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-05 19:37:41 +1000

    category syntax error fix

--- server/models/category.js
+++ server/models/category.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
 
 const { Schema } = mongoose;
-const categorySchema = new schema ({
+const categorySchema = new Schema ({
     name:{
         type: String,
         required: true,
