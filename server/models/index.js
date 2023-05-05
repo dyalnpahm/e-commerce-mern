bcd278cd82cc3e422925ca0ad48794eaf76da3f2
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-05 19:52:31 +1000

    index model

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,7 @@
-const User = require ()
+const User = require ('./user');
+const Product = require ('./product');
+const Category = require ('./category');
+const Order = require ('./order');
+
+module.exports = {User, Product, Category, Order };
+
