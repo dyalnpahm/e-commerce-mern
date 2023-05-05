aace3b929eadff711984429ee040f608bffada0e
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-05 19:48:36 +1000

    user model

---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require ('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require ('bcyrypt');
+const Order = require ('./order');
+const { schema } = require('./category');
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim:true
+    },
+    email: { 
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    orders: [Order.schema]
+});
+
+
+userSchema.pre('save', async function (next){
+    if (this.isNew || this.isModified('password')){
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+
+    }
+
+    next();
+
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+
+};
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
