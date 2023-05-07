29391d98ea6fa76cebf055b33469e0d76f07f976
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-07 19:50:48 +1000

    query and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,70 @@
+import {gql} from '@apollo/client';
+
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user{
+             _id
+            // firstName
+            // lastName
+            // email
+            // orders{
+            //     _id
+            //     purchaseDate
+            //     products{
+            //         _id
+            //         name
+            //         description
+            //         price
+            //         quantity
+            //         image
+            //     }
+            // }
+        }
+    }
+}
+`;
+
+export const ADD_ORDER = gql`
+mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+        purchaseDate
+        products{
+            _id
+            name
+            description
+            price
+            quantity
+            image
+            }
+        }
+    }
+}`;
+
+export const ADD_USER = gql`
+mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+        token
+        user{
+            _id
+            /* firstName
+            lastName
+            email
+            orders{
+                _id
+                purchaseDate
+                products{
+                    _id
+                    name
+                    description
+                    price
+                    quantity
+                    image
+                }
+            } */
+        }
+    }
+}
+`;
+
---
+++ client/src/utils/queries.js
@@ -0,0 +1,75 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_CATEGORIES = gql `
+{
+    categories {
+        _id
+        name
+    }
+}
+`;
+export const QUERY_PRODUCTS = gql `
+{
+    products {
+        _id
+        name
+        description
+        price
+        quantity
+        image
+        category {
+            _id
+            name
+        }
+    }
+}
+`;
+
+
+export const QUUERY_ALL_PRODUCTS = gql `
+{
+    products {
+        _id
+        name
+        description
+        price
+        quantity
+        image
+        category {
+            _id
+            name
+        }
+    }
+}
+`;
+
+
+export const QUERY_USER = gql `
+{
+    user {
+        firstName
+        lastName
+        email
+        orders {
+            _id
+            purchaseDate
+            products {
+                _id
+                name
+                description
+                price
+                quantity
+                image
+            }
+        }
+    }
+}
+`;
+
+export const QUERY_CHECKOUT = gql `'
+query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+        session
+    }
+}
+`;
