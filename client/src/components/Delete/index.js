b3dbf61e00d69fb6a3ce8e107c8f3a155caed8dd
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-05-08 19:48:24 +1000

    delete

---
+++ client/src/components/Delete/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Delete(props) {
+     return (
+        <span {...props} role="button" tabIndex="0">
+        ✗
+      </span>
+    );
+  }
+  
+  export default Delete;
+  
